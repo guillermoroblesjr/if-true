d9586cc7949435afe5049e6285db6da48a36b33d
Guillermo Robles Jr <will.robles@gmail.com>
2016-05-04 18:28:15 -0500

    adding logic to enable/disable submit1 button

--- js/apps/home.js
+++ js/apps/home.js
@@ -15,6 +15,7 @@ define(function(require, exports, module){
 
   var HomeSection = require('js/components/screen-sections/Home');
   var viewModels = require('js/components/viewmodels/ViewModels')();
+  var ifTrue = require('ifTrue');
 
   ////////////////////////////////////////////////////////////////////////
   // Private API
@@ -53,6 +54,31 @@ define(function(require, exports, module){
           var $el = $(event.target);
 
           console.log('homeVm: ', JSON.parse(JSON.stringify(homeVm)));
+
+          ifTrue
+            // if user selects a
+            .if( homeVm.a === true )
+            // and user selects b
+            .and(true)
+            // and does not select c
+            .and(true)
+            // and user selects 'Awesome'
+            .and(true)
+            // and time is an odd minute, i.e. 10:01 am
+            .and(true)
+            // or user enters a number between 5 and 30
+            .or(false)
+            // and number is not 10
+            .and(true)
+            // and number is not 15
+            .and(true)
+            .run(function(){
+              homeVm.submitButton1.isDisabled = false;
+            })
+            .else(function(){
+              homeVm.submitButton1.isDisabled = true;
+            });
+
         }
       },
     ],
