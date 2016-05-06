2035d90c4f4f15768b864889164ea9698c425557
Guillermo Robles Jr <will.robles@gmail.com>
2016-05-06 11:56:50 -0500

    added logic for application 2 and application 3

--- js/components/application2.js
+++ js/components/application2.js
@@ -31,12 +31,12 @@ define(function(require, exports, module){
     var extend = options.extend;
 
     ////////////////////////////////////////////////////////////////////////
-    // application1 public API
+    // application2 public API
     ////////////////////////////////////////////////////////////////////////
 
-    extend.application1 = {};
+    extend.application2 = {};
 
-    extend.application1.enableDisableSubmitButton = function( options ){
+    extend.application2.enableDisableSubmitButton = function( options ){
       'use strict';
 
       var instance = options.instance || {};
@@ -45,50 +45,14 @@ define(function(require, exports, module){
       var numberInputValue = parseInt(homeVm.numberInput);
 
       ifTrue
-        // test 1
-        .if(
-          ifTrue
-            .if( homeVm.a === true )
-            .and( homeVm.b === true )
-
-            .or( homeVm.dropdownSelection !== 'Not Awesome' )
-            .and( numberInputValue > 20 )
-        )
-        // test 2
-        .and(
-          ifTrue
-            .if( homeVm.c === false  )
-
-            .or( homeVm.d === true  )
-
-            .or( homeVm.dropdownSelection === 'Awesome' )
-        )
-        // test 3
-        .and(
-          ifTrue
-            .if( homeVm.dropdownSelection === 'Awesome' )
-
-            .or( homeVm.dropdownSelection === 'Almost Awesome' )
-        )
-        // 
-        .and( numberInputValue !== 10 )
-        .and( numberInputValue !== 29 )
-        // test 4
-        .or(
-          ifTrue
-            .if( function(){
-              return numberInputValue >= 5;
-            } )
-            .and( numberInputValue <= 30 )
-            .and( numberInputValue !== 12 )
-            .and( numberInputValue !== 29 )
-        )
+        .if( homeVm.a !== true )
+        .and( numberInputValue === 10 )
 
         .run(function(){
-          homeVm.submitButton1.isDisabled = false;
+          homeVm.submitButton2.isDisabled = false;
         })
         .else(function(){
-          homeVm.submitButton1.isDisabled = true;
+          homeVm.submitButton2.isDisabled = true;
         });
     };
 
